418ed222886caa56c986208632040eff4aacfa6f
PaulHorgan <horho@hotmail.co.uk>
2024-06-11 15:43:35 +0100

    contactus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import contactUs from './Pages/Contact';
 import './App.css';
+import { useNavigate, BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
 
 function App() {
+
+  const navigate= useNavigate();
   return (
     <div className="App">
       <header className="App-header">
        <div> NEW APP </div>
-      </header>
+       <BrowserRouter>
+       <Route path="/contactus" element ={<contactUs />} />  
+       </BrowserRouter>
+       <div onClick={navigate('/contactus')}> click here to link to contact us</div>
+       </header>
     </div>
   );
 }
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,13 @@
+function contactUs () {
+    <div className="body">
+        <div> <p> Please Enter your issue here : </p>
+        <input type="text">  </input>
+        </div>
+
+        <div> <p>Please Enter your Email address : </p>
+        <input type="text">  </input>
+        </div>
+        </div>
+}
+
+export default contactUs;
